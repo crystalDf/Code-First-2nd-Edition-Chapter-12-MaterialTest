91d1d49e895bb2421e8bebcec74a29165d6f5806
crystalDf <chendong333@gmail.com>
2018-03-01 22:24:44 +0800

    Chapter 12 MaterialTest

--- app/src/main/java/com/star/materialtest/MainActivity.java
+++ app/src/main/java/com/star/materialtest/MainActivity.java
@@ -1,7 +1,9 @@
 package com.star.materialtest;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -16,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     private Toolbar mToolbar;
     private DrawerLayout mDrawerLayout;
     private NavigationView mNavigationView;
+    private FloatingActionButton mFloatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +41,15 @@ public class MainActivity extends AppCompatActivity {
             mDrawerLayout.closeDrawers();
             return true;
         });
+
+        mFloatingActionButton = findViewById(R.id.floating_action_button);
+        mFloatingActionButton.setOnClickListener(v ->
+                Snackbar.make(v, "Data deleted", Snackbar.LENGTH_LONG)
+                        .setAction("Undo", v1 ->
+                                Toast.makeText(this, "Data restored",
+                                        Toast.LENGTH_LONG).show())
+                        .show()
+        );
     }
 
     @Override
